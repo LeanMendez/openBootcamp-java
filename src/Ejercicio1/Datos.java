df9f2a995cbc0f5d25f67f44cc4b8852eef01bf0
Leandro A. Mendez <leandroa.mendez@gmail.com>
2023-02-06 19:36:53 -0300

    First commit

---
+++ src/Ejercicio1/Datos.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+
+public class Datos {
+    public static void main(String[] args) {
+        //Data type Integer
+        byte integerVariable1= 16;
+        System.out.println(integerVariable1);
+        short integerVariable2 = 34;
+        System.out.println(integerVariable2);
+        int integerVariable3 = 25000;
+        System.out.println(integerVariable3);
+        long integerVariable4 = 67L;
+        System.out.println(integerVariable4);
+        //Data type Decimal
+        float decimalVariable1 = 4.55555f;
+        System.out.println(decimalVariable1);
+        double decimalVariable2 = 3.33333333333333d;
+        System.out.println(decimalVariable2);
+        //Data type Boolean
+        boolean booleanVariable = true;
+        System.out.println(booleanVariable);
+        //Data type text
+        char textVariable1 = 'a';
+        System.out.println(textVariable1);
+        String textVariable2 = "VAMOS MESSI";
+        System.out.println(textVariable2);
+    }
+}
